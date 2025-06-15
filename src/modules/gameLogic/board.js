26b3939ef4cf1f8482995ffd892cfc55cdf902d0
Dur4nt3 <189136258+Dur4nt3@users.noreply.github.com>
2025-06-15 22:56:42 +0300

    Add more tests to "Board" class

--- src/modules/gameLogic/board.js
+++ src/modules/gameLogic/board.js
@@ -1,11 +1,12 @@
 export default class Board {
     constructor() {
+        // Multi-dimensional array that includes 10 array of length 10
         this.grid = Array.from({ length: 10 }).map(() =>
             Array.from({ length: 10 }).map(() => true)
         );
     }
 
-    // Check if a given index pair can represent a square on the grid
+    // Check if a given index pair can represent a square on the 10x10 grid
     static isValidIndexPair(indexPair) {
         if (
             !Number.isInteger(indexPair[0]) ||
@@ -22,7 +23,7 @@ export default class Board {
         );
     }
 
-    // Translates a given algebraic notation to an index on the board
+    // Translates a given coordinate to an index on the board
     // Return value is an array of two numbers (0-9)
     // The first value is the row, the second is the column
     // Coordinate is assumed to be valid and can be validated through 'PlacementInfo.isValidCoordinate'
@@ -78,9 +79,150 @@ export default class Board {
         return true;
     }
 
-    // Marks a square with hit or miss
+    // Receive a ship type (assumed to valid, can be validated through "PlacementInfo.isValidShipType")
+    // Remove all instances of the ship from the board
+    // SHOULD NOT be used once a game has started
+    clearPlacement(type) {
+        for (const row of this.grid) {
+            for (const squareIndex in row) {
+                if (row[squareIndex] === type) {
+                    row[squareIndex] = true;
+                }
+            }
+        }
+    }
+
+    // Remove all ships from the board
+    // SHOULD NOT be used once a game has started
+    clearAll() {
+        for (const row of this.grid) {
+            for (const squareIndex in row) {
+                row[squareIndex] = true;
+            }
+        }
+    }
+
+    // Marks a square (given as an index pair) with hit or miss
+    // Expects the given index pair to be valid
     // Hit squares are arrays with the following structure '[ship type, hit]'
+    markSquare(indexPair) {
+        const squareVal = this.grid[indexPair[0]][indexPair[1]];
+        if (squareVal === true) {
+            this.grid[indexPair[0]][indexPair[1]] = 'miss';
+        } else {
+            this.grid[indexPair[0]][indexPair[1]] = [squareVal, 'hit'];
+        }
+    }
 
     // Returns true if a given square is hit
     // Expect a valid index pair
+    isHit(indexPair) {
+        // Only occupied squares that have been hit may be an array
+        return (
+            Array.isArray(this.grid[indexPair[0]][indexPair[1]]) &&
+            this.grid[indexPair[0]][indexPair[1]][1] === 'hit'
+        );
+    }
+
+    // Checks whether a given ship is destroyed
+    // Expects a valid ship type
+    isShipDestroyed(type) {
+        let count = 0;
+        for (const row of this.grid) {
+            for (const square of row) {
+                if (square === type) {
+                    count += 1;
+                }
+            }
+        }
+
+        // Because occupied squares that have been hit are converted to an array
+        // If a single occurrence of the ship type is not enclosed within this means that ship wasn't fully destroyed
+        return count === 0;
+    }
+
+    allDestroyed() {
+        let hitCount = 0;
+
+        for (const row of this.grid) {
+            for (const square of row) {
+                if (Array.isArray(square)) {
+                    if (square[1] === 'hit') {
+                        hitCount += 1;
+                    }
+                }
+            }
+        }
+
+        // 17 squares are occupied by ships
+        // If there are 17 squares that are hit => all ships are destroyed
+        return hitCount === 17;
+    }
+
+    // Checks if a ship can be place on the grid starting from a given square and a given alignment
+    // NOTE: 'startSquare' is always valid and free:
+    // 1) its pulled directly from the grid via the loop in 'getSuitablePlacements' => VALID
+    // 2) Execution of this private method isn't possible unless the square is free => FREE
+    static #isValidPlacement(startSquare, size, alignment, occupiedSquares) {
+        let currentSquare = startSquare;
+        let traverse = size - 1;
+        const range = [startSquare];
+        while (traverse > 0) {
+            if (alignment === 'v') {
+                currentSquare = [currentSquare[0] + 1, currentSquare[1]];
+            } else {
+                currentSquare = [currentSquare[0], currentSquare[1] + 1];
+            }
+
+            // Square is valid and free
+            if (
+                Board.isValidIndexPair(currentSquare) &&
+                !occupiedSquares.includes(JSON.stringify(currentSquare))
+            ) {
+                range.push(currentSquare);
+            }
+            traverse -= 1;
+        }
+
+        // Are there are enough valid and free spaces to accommodate the ship
+        return range.length === size;
+    }
+
+    // Gets all suitable locations to place a ship on the grid
+    // Expects a valid type and alignment
+    getSuitablePlacements(type, alignment) {
+        // Valid types always have their size as the last character
+        const shipSize = Number(type[type.length - 1]);
+        const occupiedSquares = this.#occupiedSquares();
+        const possiblePlacements = [];
+
+        // Iterate through the entire grid
+        for (const rowIndex in this.grid) {
+            const row = this.grid[rowIndex];
+            for (const squareIndex in row) {
+                const square = row[squareIndex];
+
+                // Skip occupied squares
+                if (square !== true) {
+                    continue;
+                }
+
+                if (
+                    Board.#isValidPlacement(
+                        [Number(rowIndex), Number(squareIndex)],
+                        shipSize,
+                        alignment,
+                        occupiedSquares
+                    )
+                ) {
+                    possiblePlacements.push([
+                        Number(rowIndex),
+                        Number(squareIndex),
+                    ]);
+                }
+            }
+        }
+
+        return possiblePlacements;
+    }
 }
--- src/modules/tests/board.test.js
+++ src/modules/tests/board.test.js
@@ -30,14 +30,15 @@ describe('Tests for Board class', () => {
     });
 
     test('Can validate that a given array is a valid index pair on the grid', () => {
-        expect.assertions(7);
+        expect.assertions(6);
 
-        expect(Board.isValidIndexPair([0, 0])).toBeTruthy();
         expect(Board.isValidIndexPair([8, 5])).toBeTruthy();
 
+        // Out of board range
         expect(Board.isValidIndexPair([10, 10])).toBeFalsy();
         expect(Board.isValidIndexPair([3, 10])).toBeFalsy();
 
+        // Invalid index pair
         expect(Board.isValidIndexPair([4.5, 6.5])).toBeFalsy();
         expect(Board.isValidIndexPair(['a', 5])).toBeFalsy();
         expect(Board.isValidIndexPair(['1', '5'])).toBeFalsy();
@@ -45,62 +46,16 @@ describe('Tests for Board class', () => {
 
     // An index on the board is a pair of row and column values in the array within the board property
     test('Can translate a coordinate to an index on the board', () => {
-        expect.assertions(3);
-
-        expect(Board.coordinateToIndex('a1')).toEqual([0, 0]);
-        expect(Board.coordinateToIndex('e5')).toEqual([4, 4]);
         expect(Board.coordinateToIndex('g8')).toEqual([6, 7]);
     });
 
     test('Can translate an index on the board to a coordinate', () => {
-        expect.assertions(3);
-
-        expect(Board.indexToCoordinate([0, 0])).toBe('a1');
-        expect(Board.indexToCoordinate([8, 3])).toBe('i4');
         expect(Board.indexToCoordinate([5, 6])).toBe('f7');
     });
 
     // Don't use 'toEqual' and try to compare to the target array (board) as a whole
     // This will massively bloat the test suite
     test('Can place ships on the board', () => {
-        expect.assertions(3);
-
-        gameBoard.placeShip('carrier5', [
-            [0, 0],
-            [0, 1],
-            [0, 2],
-            [0, 3],
-            [0, 4],
-        ]);
-
-        // Should be 5 elements that are equal to 'carrier5'
-        let carrierPlacement = [
-            gameBoard.grid[0][0],
-            gameBoard.grid[0][1],
-            gameBoard.grid[0][2],
-            gameBoard.grid[0][3],
-            gameBoard.grid[0][4],
-        ];
-
-        // If all elements are equal to 'carrier5' then the array should be empty
-        carrierPlacement = carrierPlacement.filter(
-            (element) => element !== 'carrier5'
-        );
-
-        expect(carrierPlacement.length).toBe(0);
-
-        gameBoard.placeShip('patrol2', [
-            [9, 0],
-            [9, 1],
-        ]);
-
-        let patrolPlacement = [gameBoard.grid[9][0], gameBoard.grid[9][1]];
-        patrolPlacement = patrolPlacement.filter(
-            (element) => element !== 'patrol2'
-        );
-
-        expect(patrolPlacement.length).toBe(0);
-
         gameBoard.placeShip('submarine3', [
             [3, 5],
             [4, 5],
@@ -112,6 +67,8 @@ describe('Tests for Board class', () => {
             gameBoard.grid[4][5],
             gameBoard.grid[5][5],
         ];
+
+        // If all elements are equal to 'submarine3' the ship was placed correctly
         submarinePlacement = submarinePlacement.filter(
             (element) => element !== 'submarine3'
         );
@@ -120,35 +77,12 @@ describe('Tests for Board class', () => {
     });
 
     test('Can check whether certain squares on the board are free or occupied', () => {
-        expect.assertions(7);
+        expect.assertions(2);
 
         gameBoard.placeShip('patrol2', [
             [9, 0],
             [9, 1],
         ]);
-
-        expect(
-            gameBoard.isFree([
-                [0, 0],
-                [0, 1],
-            ])
-        ).toBeTruthy();
-
-        expect(gameBoard.isFree([[1, 4]])).toBeTruthy();
-        expect(gameBoard.isFree([[9, 0]])).toBeFalsy();
-
-        expect(
-            gameBoard.isFree([
-                [9, 0],
-                [9, 1],
-            ])
-        ).toBeFalsy();
-        expect(
-            gameBoard.isFree([
-                [9, 0],
-                [0, 0],
-            ])
-        ).toBeFalsy();
 
         gameBoard.placeShip('carrier5', [
             [0, 0],
@@ -176,19 +110,338 @@ describe('Tests for Board class', () => {
         ).toBeFalsy();
     });
 
-    test.todo('Can clear a particular ship placement');
+    test('Can clear a particular ship placement', () => {
+        expect.assertions(2);
 
-    test.todo('Can reset all ship placements');
-    
-    test.todo('Can mark a given square as hit or missed');
-    
-    test.todo('Can check whether a given square is hit');
+        gameBoard.placeShip('submarine3', [
+            [3, 5],
+            [4, 5],
+            [5, 5],
+        ]);
 
-    test.todo('Can check whether a given ship is destroyed');
+        gameBoard.placeShip('patrol2', [
+            [9, 0],
+            [9, 1],
+        ]);
 
-    test.todo('Can check whether all ships are destroyed');
+        gameBoard.clearPlacement('submarine3');
 
-    test.todo('Can check for all possible placements for a ship');
+        let submarinePlacement = [
+            gameBoard.grid[3][5],
+            gameBoard.grid[4][5],
+            gameBoard.grid[5][5],
+        ];
+
+        let patrolPlacement = [gameBoard.grid[9][0], gameBoard.grid[9][1]];
+
+        submarinePlacement = submarinePlacement.filter(
+            (element) => element === true
+        );
+
+        patrolPlacement = patrolPlacement.filter((element) => element === true);
+
+        // Cleared the placement of the submarine
+        expect(submarinePlacement.length).toBe(3);
+
+        // Didn't clear the placement of the patrol
+        expect(patrolPlacement.length).toBe(0);
+    });
+
+    test('Can reset all ship placements', () => {
+        const freeSpaces = (grid) => {
+            let counter = 0;
+            for (const row of grid) {
+                for (const square of row) {
+                    if (square === true) {
+                        counter += 1;
+                    }
+                }
+            }
+            return counter;
+        };
+
+        gameBoard.placeShip('submarine3', [
+            [3, 5],
+            [4, 5],
+            [5, 5],
+        ]);
+
+        gameBoard.placeShip('patrol2', [
+            [9, 0],
+            [9, 1],
+        ]);
+
+        gameBoard.clearAll();
+
+        expect(freeSpaces(gameBoard.grid)).toBe(100);
+    });
+
+    test('Can mark a given square as hit or missed', () => {
+        gameBoard.placeShip('submarine3', [
+            [3, 5],
+            [4, 5],
+            [5, 5],
+        ]);
+
+        gameBoard.placeShip('patrol2', [
+            [9, 0],
+            [9, 1],
+        ]);
+
+        // Hits
+        gameBoard.markSquare([3, 5]);
+        gameBoard.markSquare([4, 5]);
+        gameBoard.markSquare([9, 1]);
+
+        // Misses
+        gameBoard.markSquare([9, 2]);
+        gameBoard.markSquare([3, 6]);
+        gameBoard.markSquare([6, 5]);
+
+        const hitMiss = [
+            gameBoard.grid[3][5],
+            gameBoard.grid[4][5],
+            gameBoard.grid[9][1],
+            gameBoard.grid[9][2],
+            gameBoard.grid[3][6],
+            gameBoard.grid[6][5],
+        ];
+
+        expect(hitMiss).toEqual([
+            ['submarine3', 'hit'],
+            ['submarine3', 'hit'],
+            ['patrol2', 'hit'],
+            'miss',
+            'miss',
+            'miss',
+        ]);
+    });
+
+    test('Can check whether a given square is hit', () => {
+        gameBoard.placeShip('carrier5', [
+            [0, 0],
+            [0, 1],
+            [0, 2],
+            [0, 3],
+            [0, 4],
+        ]);
+
+        gameBoard.placeShip('patrol2', [
+            [9, 0],
+            [9, 1],
+        ]);
+
+        gameBoard.markSquare([0, 2]);
+        gameBoard.markSquare([9, 2]);
+
+        const hits = [
+            // Hit
+            gameBoard.isHit([0, 2]),
+            // Marked as a miss
+            gameBoard.isHit([9, 2]),
+            // Not occupied and not missed
+            gameBoard.isHit([3, 8]),
+            // Occupied but not hit
+            gameBoard.isHit([0, 3]),
+        ];
+
+        expect(hits).toEqual([true, false, false, false]);
+    });
+
+    test('Can check whether a given ship is destroyed', () => {
+        gameBoard.placeShip('carrier5', [
+            [0, 0],
+            [0, 1],
+            [0, 2],
+            [0, 3],
+            [0, 4],
+        ]);
+
+        gameBoard.placeShip('submarine3', [
+            [3, 5],
+            [4, 5],
+            [5, 5],
+        ]);
+
+        gameBoard.placeShip('patrol2', [
+            [9, 0],
+            [9, 1],
+        ]);
+
+        gameBoard.markSquare([9, 0]);
+        gameBoard.markSquare([9, 1]);
+
+        gameBoard.markSquare([3, 5]);
+        gameBoard.markSquare([4, 5]);
+
+        const shipsDestroyed = [
+            // Patrol - Destroyed
+            gameBoard.isShipDestroyed('patrol2'),
+            // Submarine - Partially Destroyed
+            gameBoard.isShipDestroyed('submarine3'),
+            // Carrier - Not Hit
+            gameBoard.isShipDestroyed('carrier5'),
+        ];
+
+        expect(shipsDestroyed).toEqual([true, false, false]);
+    });
+
+    test('Can check whether all ships are destroyed', () => {
+        expect.assertions(2);
+
+        gameBoard.placeShip('carrier5', [
+            [0, 0],
+            [0, 1],
+            [0, 2],
+            [0, 3],
+            [0, 4],
+        ]);
+
+        gameBoard.placeShip('battleship4', [
+            [1, 0],
+            [1, 1],
+            [1, 2],
+            [1, 3],
+        ]);
+
+        gameBoard.placeShip('destroyer3', [
+            [3, 6],
+            [4, 6],
+            [5, 6],
+        ]);
+
+        gameBoard.placeShip('submarine3', [
+            [3, 5],
+            [4, 5],
+            [5, 5],
+        ]);
+
+        gameBoard.placeShip('patrol2', [
+            [9, 0],
+            [9, 1],
+        ]);
+
+        gameBoard.markSquare([3, 5]);
+        gameBoard.markSquare([4, 5]);
+        gameBoard.markSquare([5, 5]);
+
+        gameBoard.markSquare([9, 0]);
+        gameBoard.markSquare([9, 1]);
+
+        expect(gameBoard.allDestroyed()).toBeFalsy();
+
+        gameBoard.markSquare([3, 6]);
+        gameBoard.markSquare([4, 6]);
+        gameBoard.markSquare([5, 6]);
+
+        gameBoard.markSquare([0, 0]);
+        gameBoard.markSquare([0, 1]);
+        gameBoard.markSquare([0, 2]);
+        gameBoard.markSquare([0, 3]);
+        gameBoard.markSquare([0, 4]);
+
+        gameBoard.markSquare([1, 0]);
+        gameBoard.markSquare([1, 1]);
+        gameBoard.markSquare([1, 2]);
+        gameBoard.markSquare([1, 3]);
+
+        expect(gameBoard.allDestroyed()).toBeTruthy();
+    });
+
+    test('Can check for all possible placements for a ship', () => {
+        expect.assertions(2);
+
+        let rowsAndColumns = [];
+        let possiblePlacements = gameBoard.getSuitablePlacements(
+            'carrier5',
+            'h'
+        );
+        for (const placement of possiblePlacements) {
+            const coordinate = Board.indexToCoordinate(placement);
+            if (!rowsAndColumns.includes(coordinate[0])) {
+                rowsAndColumns.push(coordinate[0]);
+            }
+
+            if (!rowsAndColumns.includes(coordinate.slice(1))) {
+                rowsAndColumns.push(coordinate.slice(1));
+            }
+        }
+
+        // Squares available should be [a-j][1-6]
+        expect(rowsAndColumns).toEqual([
+            'a',
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            'b',
+            'c',
+            'd',
+            'e',
+            'f',
+            'g',
+            'h',
+            'i',
+            'j',
+        ]);
+
+        // Occupying all possible placements for the carrier on row 'a'
+        gameBoard.placeShip('carrier5', [
+            [0, 3],
+            [0, 4],
+            [0, 5],
+            [0, 6],
+            [0, 7],
+        ]);
+
+        gameBoard.placeShip('destroyer3', [
+            [1, 3],
+            [1, 4],
+            [1, 5],
+        ]);
+
+        gameBoard.placeShip('patrol2', [
+            [1, 6],
+            [1, 7],
+        ]);
+
+        rowsAndColumns = [];
+        possiblePlacements = gameBoard.getSuitablePlacements(
+            'battleship4',
+            'h'
+        );
+        for (const placement of possiblePlacements) {
+            const coordinate = Board.indexToCoordinate(placement);
+            if (!rowsAndColumns.includes(coordinate[0])) {
+                rowsAndColumns.push(coordinate[0]);
+            }
+
+            if (!rowsAndColumns.includes(coordinate.slice(1))) {
+                rowsAndColumns.push(coordinate.slice(1));
+            }
+        }
+
+        // Squares available should be [c-j][1-7]
+        expect(rowsAndColumns).toEqual([
+            'c',
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            'd',
+            'e',
+            'f',
+            'g',
+            'h',
+            'i',
+            'j',
+        ]);
+    });
 
     test.todo('Can randomly place all ships on the grid');
 });
